13c6e6fc9cfc9018ba1b3b8c98cb146500a02562
lomo74@gmail.com <lomo74>
2021-06-11 13:23:05 +0200

    Changed git attributes, added some missing files

---
+++ indybkport.hpp
@@ -0,0 +1,39 @@
+﻿// CodeGear C++Builder
+// Copyright (c) 1995, 2020 by Embarcadero Technologies, Inc.
+// All rights reserved
+
+// (DO NOT EDIT: machine generated header) 'indybkport.pas' rev: 34.00 (Windows)
+
+#ifndef IndybkportHPP
+#define IndybkportHPP
+
+#pragma delphiheader begin
+#pragma option push
+#pragma option -w-      // All warnings off
+#pragma option -Vx      // Zero-length empty class member 
+#pragma pack(push,8)
+#include <System.hpp>
+#include <SysInit.hpp>
+#include <Winapi.Windows.hpp>
+#include <System.SysUtils.hpp>
+#include <System.Classes.hpp>
+
+//-- user supplied -----------------------------------------------------------
+
+namespace Indybkport
+{
+//-- forward type declarations -----------------------------------------------
+//-- type declarations -------------------------------------------------------
+//-- var, const, procedure ---------------------------------------------------
+extern DELPHI_PACKAGE bool __fastcall TextStartsWith(const System::UnicodeString S, const System::UnicodeString SubS);
+extern DELPHI_PACKAGE void __fastcall ParseContentType(const System::UnicodeString AValue, System::UnicodeString &AContentType, System::UnicodeString &ACharSet);
+}	/* namespace Indybkport */
+#if !defined(DELPHIHEADER_NO_IMPLICIT_NAMESPACE_USE) && !defined(NO_USING_NAMESPACE_INDYBKPORT)
+using namespace Indybkport;
+#endif
+#pragma pack(pop)
+#pragma option pop
+
+#pragma delphiheader end.
+//-- end unit ----------------------------------------------------------------
+#endif	// IndybkportHPP
